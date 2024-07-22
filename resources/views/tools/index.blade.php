75a903677aff20208a921b7d98a75bb349de523d
Kaybee-debug <karabomolepo29@gmail.com>
2024-07-22 22:14:56 +0200

    modified tool view

--- resources/views/tools/index.blade.php
+++ resources/views/tools/index.blade.php
@@ -1,5 +1,346 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="page-content">
+<div class="container">
+<div class="row">
+
+<div class="col-md-12">
+<div class="marketing-section">
+<div class="marketing-content">
+<h2>Tools</h2>
+
+<div class="marketing-bg">
+<!-- <img src="assets/img/bg/market-bg.png" alt="img" class="market-bg">
+<img src="assets/img/bg/market-bg-01.png" alt="img" class="market-img"> -->
+</div>
+</div>
+</div>
+</div>
+
+
+<!-- <div class="sortby-title">
+<div class="row align-items-center">
+<div class="col-md-6">
+<h4>10 Categories found with <span>14,787</span> Services</h4>
+</div>
+<div class="col-md-6">
+<div class="search-filter-selected float-md-end">
+<div class="form-group">
+<span class="sort-text">Sort By</span>
+<select class="select select2-hidden-accessible" data-select2-id="1" tabindex="-1" aria-hidden="true">
+<option data-select2-id="3">New Arrivals</option>
+<option>Featured</option>
+</select><span class="select2 select2-container select2-container--default" dir="ltr" data-select2-id="2" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-disabled="false" aria-labelledby="select2-t0x5-container"><span class="select2-selection__rendered" id="select2-t0x5-container" role="textbox" aria-readonly="true" title="New Arrivals">New Arrivals</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+</div>
+</div>
+</div>
+</div>
+</div> -->
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="https://radapps.co.za/digiinc/images/2024/02/09/close-up-colleague-work_23-2148894048.jpg" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$4,580</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Agricultural Tech</a></h4>
+<!-- <span>7860 Sevices</span> -->
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="https://radapps.co.za/digiinc/images/2024/02/09/engineer-data-center-using-tablet-prevent-system-overload-close-up-employee-server-room-ensuring-there-is-enough-network-connectivity-smooth-opera.jpg" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$5,620</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Financial Tech</a></h4>
+<!-- <span>4709 Sevices</span> -->
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="https://radapps.co.za/digiinc/images/2024/02/09/focused-black-female-student-describing-vr-experience_74855-1880.jpg
+" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$4,310</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Educational Tech</a></h4>
+<!-- <span>1590 Sevices</span> -->
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="	https://radapps.co.za/digiinc/images/2024/02/09/ai-tech.jpg" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$2,780</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">AI Tech</a></h4>
+<!-- <span>1458 Sevices</span> -->
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="	https://radapps.co.za/digiinc/images/2024/02/09/coding-and-app-development-tech.jpg
+" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$8,060</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Coding and App development Tech</a></h4>
+<!-- <span>5678 Sevices</span> -->
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="https://radapps.co.za/digiinc/images/2024/02/09/businessman-checking-his-phone_53876-93007.jpg" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$2,130</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Cybersecurity</a></h4>
+<!-- <span>1874 Sevices</span> -->
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="https://radapps.co.za/digiinc/images/2024/02/09/robotics-tech.jpg" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$4,910</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Robotics Tech</a></h4>
+<!-- <span>2478 Sevices</span> -->
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<!-- <div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="https://radapps.co.za/digiinc/images/2024/02/09/robotics-tech.jpg" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$3,040</span> -->
+<!-- </div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Agri Tech</a></h4>
+<!-- <span>1454 Sevices</span> -->
+<!-- </div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div> --> 
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="	https://radapps.co.za/digiinc/images/2024/02/09/digital-wardrobe-transparent-screen_53876-105380.jpg" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$2,850</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Ecommerce Tech</a></h4>
+<span>1274 Sevices</span>
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="https://radapps.co.za/digiinc/images/2024/02/09/saas-concept-collage_23-2149399281.jpg" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$2,850</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Cloud Tech</a></h4>
+<span>1874 Sevices</span>
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="https://radapps.co.za/digiinc/images/2024/02/09/black-woman-mechanical-industry-working-late-night-doing-overtime-start-up-office_482257-5385.jpg
+" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$4,950</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Design Tech</a></h4>
+<span>2478 Sevices</span>
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="service-grid">
+<div class="service-img">
+<a href="service.html">
+<img src="https://radapps.co.za/digiinc/images/2024/02/09/young-man-recording-woman-singing-radio-station_23-2148815383.jpg" class="img-fluid" alt="img">
+</a>
+<div class="avg-price">
+<!-- <h6>Average Price</h6>
+<span>$6,580</span> -->
+</div>
+</div>
+<div class="service-type d-flex justify-content-between align-items-center">
+<div class="servive-name">
+<h4><a href="service.html">Video and Sound Tech</a></h4>
+<!-- <span>1454 Sevices</span> -->
+</div>
+<div class="next-arrow">
+<a href="service.html"><i class="feather-arrow-up-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="search-load-btn">
+<a href="javascript:void(0);" class="btn btn-secondary">
+<img src="assets/img/icons/loader-icon-01.svg" class="spinner-border" alt="Icon"> Load More
+</a>
+</div>
+
+</div>
+</div>
+</div>
+
+
+
 @include('components.footer')
 @endsection
