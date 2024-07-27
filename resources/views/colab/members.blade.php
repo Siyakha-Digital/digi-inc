adf20479e6435fe4295d761866135915a3cbb30b
Enhlekhoza <dzimbaelsa3@gmail.com>
2024-07-27 12:10:03 +0200

    push

---
+++ resources/views/colab/members.blade.php
@@ -0,0 +1,109 @@
+<div class="joms-module__wrapper"><div class="joms-tab__bar">
+    <a href="#joms-app--129" class="active">
+    JS Members        </a>
+</div>
+
+<div id="joms-app--129" class="joms-tab__content">
+    <div class="joms-module">    <div class="joms-js--member-module">
+<div class="joms-gap"></div>
+<ul class="joms-list--thumbnail clearfix">
+
+                    <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/119-siyabonga">
+                        <img src="https://radapps.co.za/digiinc/images/avatar/thumb_3fdff448767f45551b3a2996.jpg" title="Siyabonga" alt="Siyabonga" data-author="119">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/118-zorro">
+                        <img src="https://radapps.co.za/digiinc/components/com_community/assets/user-Male-thumb.png" title="Zorro" alt="Zorro" data-author="118">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/117-roboto">
+                        <img src="https://radapps.co.za/digiinc/components/com_community/assets/user-Male-thumb.png" title="Roboto" alt="Roboto" data-author="117">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/116-flamingo">
+                        <img src="https://radapps.co.za/digiinc/images/avatar/thumb_8dac66e795b4c72b133c7fb2.png" title="Flamingo" alt="Flamingo" data-author="116">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/115-clare">
+                        <img src="https://radapps.co.za/digiinc/components/com_community/assets/user-Male-thumb.png" title="Clare" alt="Clare" data-author="115">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/114-kingsley">
+                        <img src="https://radapps.co.za/digiinc/components/com_community/assets/user-Male-thumb.png" title="Kingsley" alt="Kingsley" data-author="114">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/113-zero">
+                        <img src="https://radapps.co.za/digiinc/images/avatar/thumb_0d3aea357f1befbda94e6e10.jpg" title="Zero" alt="Zero" data-author="113">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/112-loni-hutchison">
+                        <img src="https://radapps.co.za/digiinc/components/com_community/assets/user-Male-thumb.png" title="Loni Hutchison" alt="Loni Hutchison" data-author="112">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/111-lerato">
+                        <img src="https://radapps.co.za/digiinc/components/com_community/assets/user-Male-thumb.png" title="Lerato" alt="Lerato" data-author="111">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/110-kamogelo-ngwenya">
+                        <img src="https://radapps.co.za/digiinc/images/avatar/thumb_d5d33adc6fb566144ef37670.jpg" title="Kamogelo Ngwenya " alt="Kamogelo Ngwenya " data-author="110">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/109-amo-marengwa">
+                        <img src="https://radapps.co.za/digiinc/components/com_community/assets/user-Male-thumb.png" title="Amo Marengwa" alt="Amo Marengwa" data-author="109">
+                    </a>
+                </div>
+            </li>
+                            <li class="joms-list__item joms-list__item--0">
+                <div class="joms-avatar ">
+                    <a href="/digiinc/index.php/cowork/profile/edit-profile/108-tanatswa">
+                        <img src="https://radapps.co.za/digiinc/components/com_community/assets/user-Male-thumb.png" title="Tanatswa" alt="Tanatswa" data-author="108">
+                    </a>
+                </div>
+            </li>
+            </ul>
+</div>
+
+<div class="joms-gap"></div>
+<a href="/digiinc/index.php/colab/search/browse" class="joms-button--link">
+<small>
+View All (17)    </small>
+</a>
+
+</div>        </div>
+
+</div>
+
+
+</div>
---
+++ resources/views/colab/women-in-tech.blade.php
@@ -0,0 +1,94 @@
+<div class="joms-stream__body">
+    <div class="joms-media like">
+        <a href="/digiinc/index.php/colab/events/viewevent/1-women-in-tech-experience-2023">
+            <div class="joms-media__cover">
+                
+                <div class="joms-focus__date cover">
+                    <span>Oct</span>
+                    <span>18</span>
+                </div>
+                <img src="https://radapps.co.za/digiinc/components/com_community/assets/cover-event.png" alt="Women in Tech Experience 2023">
+            </div>
+        </a>
+        <h4 class="joms-text--title"><a href="/digiinc/index.php/colab/events/viewevent/1-women-in-tech-experience-2023">Women in Tech Experience 2023</a></h4>
+        <p class="joms-text--desc">This year, the unique event calls on women from all corners of the tech world to unite and join the SHEvolution movement</p>
+        <ul class="joms-list">
+            <li class="joms-list__item">
+                <svg viewBox="0 0 16 18" class="joms-icon">
+                    <use xlink:href="https://radapps.co.za/digiinc/index.php/colab#joms-icon-calendar"></use>
+                </svg>
+            Wednesday, 18 October 2023 09:00 AM</li>
+            <li class="joms-list__item">
+                <svg viewBox="0 0 16 16" class="joms-icon">
+                    <use xlink:href="https://radapps.co.za/digiinc/index.php/colab#joms-icon-location"></use>
+                </svg>
+            Johanessburg, South Africa</li>
+        </ul>
+    </div>
+    </div>            </div>
+                                            <div class="joms-stream joms-embedly--left joms-js--stream joms-js--stream-6 " data-stream-id="6" data-stream-type="events">
+                <div class="joms-list__options">
+    
+    
+    <ul class="joms-dropdown">
+        
+        
+        
+       
+        
+    </ul>
+    
+    </div>
+    
+        
+    <div class="joms-stream__header">
+    <div class="joms-avatar--stream ">
+        <a href="/digiinc/index.php/cowork/profile/edit-profile/103-administrator">
+            <img data-author="103" src="https://radapps.co.za/digiinc/components/com_community/assets/user-Male-thumb.png" alt="Digi Inc">
+        </a>
+    </div>
+    <div class="joms-stream__meta">
+                <a href="/digiinc/index.php/cowork/profile/edit-profile/103-administrator" class="joms-stream__user">Digi Inc</a>
+                 added a new event        <span class="joms-stream__time"><small>
+            9 months ago         </small></span>
+    </div>
+    </div>
+    <div class="joms-stream__body">
+    
+    <div class="joms-media">
+        <h4 class="joms-text--title"><a href="/digiinc/index.php/colab/events/viewevent/1-women-in-tech-experience-2023">Women in Tech Experience 2023</a></h4>
+        <p>This year, the unique event calls on women from all corners of the tech world to unite and join the SHEvolution movement, envisioning a world where...</p>
+        <ul class="joms-list">
+            <li class="joms-list__item">
+                <svg viewBox="0 0 16 18" class="joms-icon">
+                    <use xlink:href="https://radapps.co.za/digiinc/index.php/colab#joms-icon-calendar"></use>
+                </svg>
+            Wednesday, 18 October 2023 09:00 AM</li>
+            <li class="joms-list__item">
+                <svg viewBox="0 0 16 16" class="joms-icon">
+                    <use xlink:href="https://radapps.co.za/digiinc/index.php/colab#joms-icon-location"></use>
+                </svg>
+            Johanessburg, South Africa</li>
+        </ul>
+    </div>
+    
+    </div>
+    
+    <div class="joms-stream__actions">
+        <!-- share -->
+        
+    </div>
+    
+                    <div class="joms-stream__status">
+            <div class="joms-stream__reactions">
+    <ul class="joms-reactions__list">
+            <li style="z-index: 100;">
+                        <div class="joms-reactions__item reaction-like" title="1 people" data-count="1" data-reactid="1" data-uid="1" onclick="joms.popup.reaction('events', 1, 1)">
+            </div>
+        </li>
+        </ul>
+        <a class="joms-stream__reactions-text" href="javascript:;" onclick="joms.popup.reaction('events', 1)">
+        Taks the Dev</a>
+    </div>
+    
+        </div>
